1b0d2abded6a8fb83c82833e8bfdeeb96ca44fab
Andy <96415472+ydalosa@users.noreply.github.com>
2022-12-09 06:55:15 +0100

    Update A11_1_LinearLayoutVerticalXml.java

--- app/src/main/java/com/dam/snippets/a1_layout_xml/A11_1_LinearLayoutVerticalXml.java
+++ app/src/main/java/com/dam/snippets/a1_layout_xml/A11_1_LinearLayoutVerticalXml.java
@@ -1,16 +1,44 @@
-package com.dam.snippets.a1_layout_xml;
+package com.example.intentdata1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.dam.snippets.R;
-
-public class A11_1_LinearLayoutVerticalXml extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_a111_linear_layout_vertical_xml);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("First Activity");
+
+        EditText mNameEt = findViewById(R.id.nameEt);
+        EditText mEmailEt = findViewById(R.id.emailEt);
+        EditText mPhoneEt = findViewById(R.id.phoneEt);
+
+        Button mSaveBtn = findViewById(R.id.saveBtn);
+
+        mSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameEt.getText().toString();
+                String email = mEmailEt.getText().toString();
+                String phone = mPhoneEt.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("NAME", name);
+                intent.putExtra("EMAIL", email);
+                intent.putExtra("PHONE", phone);
+
+                startActivity(intent);
+            }
+        });
     }
 }
